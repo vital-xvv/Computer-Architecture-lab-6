87f3f447b5af1327229ec12975ed3f79f7316be9
vital-xvv <vital.xvv@gmail.com>
2023-01-06 10:09:07 +0200

    Lab 6 upload

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vital-xvv/Desktop/test2/hello.ko := ccache arm-linux-gnueabihf-ld -r -EL -z noexecstack -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/vital-xvv/Desktop/test2/hello.ko /home/vital-xvv/Desktop/test2/hello.o /home/vital-xvv/Desktop/test2/hello.mod.o ;  true
---
+++ .tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/vital-xvv/Desktop/test2/hello.ko
+/home/vital-xvv/Desktop/test2/hello.o
+
